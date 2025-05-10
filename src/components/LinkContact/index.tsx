f41a616919624aa53137d2395d650778553faaa9
Weslem Vitor <playn467@gmail.com>
2025-05-10 01:51:09 -0300

    :recycle: refactor(link-contact): added my links to contact

--- src/components/LinkContact/index.tsx
+++ src/components/LinkContact/index.tsx
@@ -5,14 +5,18 @@ import Link from "next/link";
 export default function CardContact() {
   return (
     <div className="flex gap-4">
-      <Link href="https://github.com" target="_blank" rel="noopener noreferrer">
+      <Link
+        href="https://github.com/vitanho98"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Button variant="ghost" size="icon" className="cursor-pointer">
           <Github className="h-5 w-5" />
           <span className="sr-only">Github</span>
         </Button>
       </Link>
       <Link
-        href="https://linkedin.com"
+        href="http://linkedin.com/in/weslem-vitor"
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -22,7 +26,7 @@ export default function CardContact() {
         </Button>
       </Link>
       <Link
-        href="mailto:email@example.com"
+        href="mailto:weslemstorl@gmail.com"
         target="_blank"
         rel="noopener noreferrer"
       >
